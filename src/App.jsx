d908582fc755f3b3cc26208546d62b89c0c04b4b
jonnydevs <jonnydevs.info@gmail.com>
2023-05-09 16:06:35 +0300

    Complete responsive design.

--- src/App.jsx
+++ src/App.jsx
@@ -22,32 +22,32 @@ import john from "./assets/john.png";
 import lara from "./assets/lara.png";
 import space from "./assets/space.png";
 import hand from "./assets/hand.png";
-import lady from "./assets/lady.png"
-import boy from "./assets/boy.png"
-import crazy from "./assets/crazy.png"
-import image1 from "./assets/image1.png"
-import image2 from "./assets/image2.png"
-import image3 from "./assets/image3.png"
-import image4 from "./assets/image4.png"
-import image5 from "./assets/image5.png"
-import image6 from "./assets/image6.png"
-import image7 from "./assets/image7.png"
-import image8 from "./assets/image8.png"
-import image9 from "./assets/image9.png"
-import image10 from "./assets/image10.png"
-import image11 from "./assets/image11.png"
-import image12 from "./assets/image12.png"
+import lady from "./assets/lady.png";
+import boy from "./assets/boy.png";
+import crazy from "./assets/crazy.png";
+import image1 from "./assets/image1.png";
+import image2 from "./assets/image2.png";
+import image3 from "./assets/image3.png";
+import image4 from "./assets/image4.png";
+import image5 from "./assets/image5.png";
+import image6 from "./assets/image6.png";
+import image7 from "./assets/image7.png";
+import image8 from "./assets/image8.png";
+import image9 from "./assets/image9.png";
+import image10 from "./assets/image10.png";
+import image11 from "./assets/image11.png";
+import image12 from "./assets/image12.png";
 
 function App() {
   return (
-    <div className=" bg-slate-950 text-white pb-[100px]">
-      <div className="bg-[url('./assets/bg.png')] bg-no-repeat bg-center bg-fill h-[100vh] text-white p-[40px] px-[100px] w-full">
-        <div className=" flex justify-between w-full">
+    <div className=" bg-slate-950 text-white pb-[50px]">
+      <div className="bg-[url('./assets/bg.png')] bg-no-repeat bg-center  text-white p-[40px] lg:px-[100px] w-full">
+        <div className=" flex justify-between items-center w-full">
           <div className=" flex align-middle gap-4">
             <img src={logo} alt="" className=" w-8 h-8" />
             <h1>NFTs</h1>
           </div>
-          <div className=" flex gap-[130px] items-center">
+          <div className=" hidden lg:flex gap-[130px] items-center">
             <div className=" flex gap-12">
               <a href="#">Home</a>
               <a href="#">Market</a>
@@ -67,10 +67,10 @@ function App() {
           </div>
         </div>
         <div className=" mt-[30px]">
-          <h1 className=" w-[450px] h-[240px] font-[700] text-[54px]">
+          <h1 className=" font-[700] text-[54px] sm:w-[450px]">
             Discover, Collect and Sell Dope NFTs
           </h1>
-          <p className=" mt-8 w-[526px]">
+          <p className=" mt-8 sm:w-[526px]">
             Lorem ipsum dolor sit amet, consectetur adipisc elit amet.
             Consectetu at it lrorem ipsum dolor sit amet.
           </p>
@@ -81,34 +81,34 @@ function App() {
             >
               Explore
             </a>
-            <div class=" w-[116px] h-[53px] bg-gradient-to-r from-[#6578DD] via-[#B35BC7] to-[#DC1DC4] p-1 cursor-pointer">
-              <div class="flex w-full h-full bg-slate-900 items-center justify-center">
-                <h1 class=" text-base text-white">Create</h1>
+            <div className=" w-[116px] h-[53px] bg-gradient-to-r from-[#6578DD] via-[#B35BC7] to-[#DC1DC4] p-1 cursor-pointer">
+              <div className="flex w-full h-full bg-slate-900 items-center justify-center">
+                <h1 className=" text-base text-white">Create</h1>
               </div>
             </div>
           </div>
-          <div className=" flex justify-between items-center w-[500px] h-[100px] bg-[rgba(255,255,255,0.1)] px-12">
+          <div className=" flex justify-between items-center w-full h-[100px] bg-[rgba(255,255,255,0.1)] px-2 sm:w-[500px] sm:px-12">
             <div>
-              <h1 className=" font-[700] text-4xl">80K+</h1>
+              <h1 className=" font-[700] text-2xl">80K+</h1>
               <p className=" font-[400] text-base">Active user</p>
             </div>
             <div>
-              <div className=" font-[700] text-4xl">27K+</div>
+              <div className=" font-[700] text-2xl">27K+</div>
               <p className=" font-[400] text-base">Artworks</p>
             </div>
             <div>
-              <h1 className=" font-[700] text-4xl">3.5K+</h1>
+              <h1 className=" font-[700] text-2xl">3.5K+</h1>
               <p className=" font-[400] text-base">Artists</p>
             </div>
           </div>
         </div>
       </div>
-      <div className=" py-[124px] px-[100px]">
+      <div className=" py-[24px] px-6 lg:px-[100px] lg:py-[124px]">
         <img src={frame} alt="" />
       </div>
-      <div className=" px-[100px] flex items-center">
+      <div className=" px-6 lg:px-[100px] lg:flex items-center overflow-hidden lg:pb-[100px]">
         <div>
-          <h1 className=" text-5xl font-bold mb-[20px]">How it Works</h1>
+          <h1 className=" text-2xl font-bold mb-[20px] lg:text-4xl">How it Works</h1>
           <p className=" w-[386px] text-base font-normal mb-[32px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit amet. Lorem
             ipsum dolor sit amet, consecte adipiscing elit ipsum.
@@ -120,7 +120,7 @@ function App() {
             Learn More
           </a>
         </div>
-        <div className=" grid grid-cols-2 gap-8">
+        <div className=" grid grid-cols-1 mt-8 gap-8 md:grid-cols-2">
           <div className=" bg-gradient-to-r from-[#7472D9] via-[#A560CB] to-slate-950 p-1 h-full w-full">
             <div className=" h-full w-full bg-slate-950 p-8">
               <div className=" bg-[rgba(255,255,255,0.12)] w-[60px] h-[60px] flex justify-center items-center rounded mb-[38px]">
@@ -167,11 +167,11 @@ function App() {
           </div>
         </div>
       </div>
-      <div className=" p-[100px]">
-        <div className=" flex justify-between items-end mb-9">
+      <div className=" p-4 lg:px-[100px]">
+        <div className="  justify-between items-end mb-9">
           <div>
-            <h1 className=" text-5xl font-bold">Trending Auctions</h1>
-            <p className=" text-base font-normal w-[526px] mt-3">
+            <h1 className=" text-2xl font-bold lg:text-4xl">Trending Auctions</h1>
+            <p className=" text-base font-normal my-3">
               Lorem ipsum dolor sit amet, consectetur adipisc elit amet.
               Consectetu at it lrorem ipsum dolor sit amet.
             </p>
@@ -185,8 +185,8 @@ function App() {
             </div>
           </div>
         </div>
-        <div className=" flex justify-between w-full">
-          <div className=" flex flex-col gap-4 p-6 border-2 border-slate-600 h-[492px] w-[354px]">
+        <div className="flex flex-col gap-6 justify-between w-full lg:flex-row">
+          <div className=" flex flex-col gap-4 p-6 border-2 border-slate-600 h-[492px] w-full">
             <div className=" flex justify-between items-end">
               <h2 className=" font-bold text-lg">VERUALY ART</h2>
               <span className=" text-sm font-normal">@nftsart</span>
@@ -194,35 +194,43 @@ function App() {
             <img src={demon} alt="" className=" h-[75%] w-full" />
             <div className=" flex justify-between">
               <div>
-                <h2 className=" font-bold text-[20px]">15h : 05m : 26s</h2>
+                <h2 className=" font-bold text-[16px]">15h : 05m : 26s</h2>
                 <p className=" font-normal text-sm">Remaining Time</p>
               </div>
               <div>
-                <h2 className=" font-bold text-[20px]">20.25 ETH</h2>
+                <h2 className=" font-bold text-[16px]">20.25 ETH</h2>
                 <p className=" font-normal text-sm">Highest bid</p>
               </div>
             </div>
           </div>
-          <div className=" flex flex-col gap-4 p-6 border-[0.5px] border-[rgba(255, 255,255,0.28)] border-opacity-0 h-[492px] w-[354px]">
+          <div className=" flex flex-col gap-4 p-6 border-[0.5px] border-[rgba(255, 255,255,0.28)] border-opacity-0 h-[492px] w-full">
             <div className=" flex justify-between items-end">
               <h2 className=" font-bold text-lg">VERUALY ART</h2>
               <span className=" text-sm font-normal">@nftsart</span>
             </div>
             <div className=" flex gap-6">
-              <img src={puppy} alt="" className=" h-full w-2/3" />
+              <img src={puppy} alt="" className=" h-[300px] w-2/3" />
               <div className=" flex flex-col h-full justify-between items-center">
-                <img src={casual} alt="" className=" w-10 h-10" />
-                <img src={heart} alt="" className=" w-10 h-10" />
-                <img src={network} alt="" className=" w-10 h-10" />
+                <img
+                  src={casual}
+                  alt=""
+                  className=" w-10 h-10 sm:w-20 sm:h-20"
+                />
+                <img src={heart} alt="" className=" w-10 h- sm:w-20 sm:h-20" />
+                <img
+                  src={network}
+                  alt=""
+                  className=" w-10 h-10 sm:w-20 sm:h-20"
+                />
               </div>
             </div>
             <div className=" flex justify-between">
               <div>
-                <h2 className=" font-bold text-[20px]">15h : 05m : 26s</h2>
+                <h2 className=" font-bold text-[16px]">15h : 05m : 26s</h2>
                 <p className=" font-normal text-sm">Remaining Time</p>
               </div>
               <div>
-                <h2 className=" font-bold text-[20px]">20.25 ETH</h2>
+                <h2 className=" font-bold text-[16px]">20.25 ETH</h2>
                 <p className=" font-normal text-sm">Highest bid</p>
               </div>
             </div>
@@ -230,7 +238,7 @@ function App() {
               Place a bid
             </button>
           </div>
-          <div className=" flex flex-col gap-4 p-6 border-2 border-slate-600 h-[492px] w-[354px]">
+          <div className=" flex flex-col gap-4 p-6 border-2 border-slate-600 h-[492px] w-full">
             <div className=" flex justify-between items-end">
               <h2 className=" font-bold text-lg">VERUALY ART</h2>
               <span className=" text-sm font-normal">@nftsart</span>
@@ -238,28 +246,28 @@ function App() {
             <img src={dog} alt="" className=" h-[75%] w-full" />
             <div className=" flex justify-between">
               <div>
-                <h2 className=" font-bold text-[20px]">15h : 05m : 26s</h2>
+                <h2 className=" font-bold text-[16px]">15h : 05m : 26s</h2>
                 <p className=" font-normal text-sm">Remaining Time</p>
               </div>
               <div>
-                <h2 className=" font-bold text-[20px]">20.25 ETH</h2>
+                <h2 className=" font-bold text-[16px]">20.25 ETH</h2>
                 <p className=" font-normal text-sm">Highest bid</p>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className=" p-[100px] pt-0">
+      <div className="p-4 lg:p-[100px] pt-0">
         <div className=" flex justify-between items-end">
-          <h1 className=" font-bold text-5xl">Top Seller</h1>
+          <h1 className=" font-bold text-2xl lg:text-4xl">Top Seller</h1>
           <a href="#" className=" underline">
             View all
           </a>
         </div>
-        <div className=" flex gap-6 mt-[50px]">
+        <div className=" flex flex-col gap-6 mt-[50px] sm:grid sm:grid-cols-2 lg:grid-cols-3">
           <div
             className=" flex items-center justify-center gap-4 bg-gradient-to-r from-[#7073DA] via-[#B15CC8]
-            to-[#D828C3] w-[468px] h-[160px]"
+            to-[#D828C3] w-full h-[160px]"
           >
             <img src={alex} alt="" />
             <div>
@@ -269,7 +277,7 @@ function App() {
           </div>
           <div
             className=" flex items-center justify-center gap-4 bg-gradient-to-r from-[#846DD6] via-[#AD5FCA]
-            to-[#C345C7] w-[468px] h-[160px]"
+            to-[#C345C7] w-full h-[160px]"
           >
             <img src={john} alt="" />
             <div>
@@ -279,7 +287,7 @@ function App() {
           </div>
           <div
             className=" flex items-center justify-center gap-4 bg-gradient-to-r from-[#876CD5] via-[#AA60CB]
-            to-[#CB3AC5] w-[468px] h-[160px]"
+            to-[#CB3AC5] w-full h-[160px]"
           >
             <img src={lara} alt="" />
             <div>
@@ -287,11 +295,9 @@ function App() {
               <p>400 Artworks</p>
             </div>
           </div>
-        </div>
-        <div className=" flex gap-6 ml-8 overflow-hidden mt-[30px]">
           <div
             className=" flex items-center justify-center gap-4 bg-gradient-to-r from-[#7073DA] via-[#B15CC8]
-            to-[#D828C3] w-[468px] h-[160px]"
+            to-[#D828C3] w-full h-[160px]"
           >
             <img src={alex} alt="" />
             <div>
@@ -301,7 +307,7 @@ function App() {
           </div>
           <div
             className=" flex items-center justify-center gap-4 bg-gradient-to-r from-[#846DD6] via-[#AD5FCA]
-            to-[#C345C7] w-[468px] h-[160px]"
+            to-[#C345C7] w-full h-[160px]"
           >
             <img src={john} alt="" />
             <div>
@@ -311,7 +317,7 @@ function App() {
           </div>
           <div
             className=" flex items-center justify-center gap-4 bg-gradient-to-r from-[#876CD5] via-[#AA60CB]
-            to-[#CB3AC5] w-[468px] h-[160px]"
+            to-[#CB3AC5] w-full h-[160px]"
           >
             <img src={lara} alt="" />
             <div>
@@ -321,17 +327,19 @@ function App() {
           </div>
         </div>
       </div>
-      <div className=" px-[100px] mb-[100px]">
-        <div className=" flex justify-between items-end">
+      <div className="px-4 lg:px-[100px] mb-[100px]">
+        <div className=" flex flex-col lg:flex-row justify-between">
           <div>
-            <h1 className=" font-bold text-5xl mb-3">Hot Collections</h1>
+            <h1 className=" font-bold text-2xl lg:text-5xl mb-3">
+              Hot Collections
+            </h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit amet.</p>
           </div>
-          <a href="#" className=" underline">
+          <a href="#" className=" underline self-end">
             View all
           </a>
         </div>
-        <div className=" flex gap-4 mt-[68px]">
+        <div className=" flex flex-col items-center gap-4 mt-[68px] sm:grid sm:grid-cols-2 lg:grid-cols-3 lg:place-content-center xl:grid-cols-4">
           <div className=" flex flex-col gap-4 p-6 border-2 border-slate-900 h-[412px] w-[282px]">
             <img src={crazy} alt="" className=" w-full" />
             <div className=" grid grid-cols-3 w-full gap-2">
@@ -402,35 +410,31 @@ function App() {
           </div>
         </div>
       </div>
-      <div className=" bg-slate-800 mb-[100px] h-[352px] mx-[192px] p-12 flex relative">
+      <div className=" bg-slate-800 mb-[100px] mx-10 lg:mx-[192px] p-2 flex flex-col lg:flex-row lg:p-6">
         <div>
-          <h1 className=" font-bold text-[42px]">Subscribe Our Newsletters</h1>
-          <p className="font-normal text-base w-[594px] mt-[20px] mb-[40px]">
+          <h1 className=" font-bold text-[32px]">Subscribe Our Newsletters</h1>
+          <p className="font-normal text-base mt-[20px] mb-[40px]">
             Lorem ipsum dolor sit amet, consectetur adipisc elit amet ipsum
             dolor sit amet, consectetur adipisc elit. Lorem ipsum dolor sit
             amet.
           </p>
-          <div className=" flex h-[56px] w-[547px]">
+          <div className=" flex items-center justify-center h-[36px] w-full mb-9 lg:w-[547px]">
             <input
               type="email"
               placeholder="Enter your email..."
-              className=" py-2 px-6 bg-slate-700 h-full"
+              className=" bg-slate-700 h-full px-2 outline-none border-none lg:w-full lg:px-6 lg:py-3"
             />
             <div
               href="#"
-              className=" bg-gradient-to-r from-[#7273D9] via-[#B35BC7] to-[#D927C3] py-3 px-6 h-full w-fit"
+              className=" flex justify-center items-center px-2 bg-gradient-to-r from-[#7273D9] via-[#B35BC7] to-[#D927C3] h-full cursor-pointer"
             >
               Subscribe
             </div>
           </div>
         </div>
-        <img
-          src={space}
-          alt=""
-          className=" w-[338px] h-[342px] absolute bottom-5 right-4"
-        />
+        <img src={space} alt="" className="sm:w-1/3 sm:mx-auto" />
       </div>
-      <footer className=" flex justify-between items-center px-[100px]">
+      <footer className=" flex flex-col justify-between p-4 px-[50px] gap-8 lg:px-[100px] sm:grid sm:grid-cols-2 lg:flex lg:flex-row lg:justify-between">
         <div className=" flex flex-col gap-3">
           <h2 className=" font-bold text-2xl ">Marketplace</h2>
           <p className=" font-normal text-base">All NFTs</p>
@@ -454,7 +458,7 @@ function App() {
         </div>
         <div className=" flex flex-col gap-3">
           <h2 className=" font-bold text-2xl ">Newsletter</h2>
-          <p className=" w-[314px] h-[44px] font-normal text-base ">
+          <p className=" w-[300px] h-[44px] font-normal text-base ">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit amet.
           </p>
           <div className=" flex gap-[20px] mt-[20px]">
